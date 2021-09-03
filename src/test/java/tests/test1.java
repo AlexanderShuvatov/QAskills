43d48e5a1a5324e96028bc8cd4a77846f5caf03a
AlexanderShuvatov <ShuvatovAlexander@yandex.ru>
2021-09-03 12:31:05 +0400

    Добавлен расчет через калькулятор прокликиванием кнопок

--- src/test/java/tests/test1.java
+++ src/test/java/tests/test1.java
@@ -1,6 +1,8 @@
 package tests;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -11,7 +13,22 @@ public class test1 {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("http://google.com");
-        driver.quit();
+        driver.findElement(By.cssSelector("input.gLFyf.gsfi")).sendKeys("Калькулятор", Keys.ENTER);
+        driver.findElement(By.cssSelector("div [jsname='j93WEe']")).click();
+        driver.findElement(By.cssSelector("div [jsname='N10B9']")).click();
+        driver.findElement(By.cssSelector("div [jsname='XSr6wc']")).click();
+        driver.findElement(By.cssSelector("div [jsname='lVjWed']")).click();
+        driver.findElement(By.cssSelector("div [jsname='qCp9A']")).click();
+        driver.findElement(By.cssSelector("div [jsname='YovRWb']")).click();
+        driver.findElement(By.cssSelector("div [jsname='KN1kY']")).click();
+        driver.findElement(By.cssSelector("div [jsname='pPHzQc']")).click();
+        driver.findElement(By.cssSelector("div [jsname='xAP7E']")).click();
+        driver.findElement(By.cssSelector("div [jsname='bkEvMb']")).click();
+        driver.findElement(By.cssSelector("div [jsname='WxTTNd']")).click();
+        driver.findElement(By.cssSelector("div [jsname='Ax5wH']")).click();
+        driver.findElement(By.cssSelector("div [jsname='Pt8tGc']")).click();
 
-    }
+
+        driver.quit();
+           }
 }
